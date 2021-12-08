836479c7320e092b7f1899c274856b17abfca7d0
Killassa <gregory.reymermet@gmail.com>
2021-12-08 13:11:23 +0100

    Modification des operator pour réalisation selon la donnée et somme des colonnes

--- matrice.cpp
+++ matrice.cpp
@@ -11,27 +11,31 @@ Compilateur    : Mingw-w64 g++ 11.2.0
 */
 
 #include <algorithm>
+#include <numeric>
 #include <iostream>
+
 #include "matrice.h"
 
-ostream& operator << (ostream& os, const Vecteur1D& vecteur) {
-      os << "[";
-      for (size_t i=0; i<vecteur.size(); ++i) {
-         if (i)
-            os << ", ";
-         os << vecteur[i];
-      }
-      os << "]";
-      return os;
-   }
+int addition(int a, int b);
+Vecteur1D sommeVecteur(const Vecteur1D& v1, const Vecteur1D& v2);
 
+ostream& operator << (ostream& os, const Vecteur1D& vecteur) {
+   os << "(";
+   for (Vecteur1D::const_iterator it = vecteur.begin(); it < vecteur.end(); ++it) {
+      if (it != vecteur.begin())
+         os << ", ";
+      os << *it;
+   }
+   os << ")";
+   return os;
+}
 
 ostream& operator << (ostream& os, const Matrice& matrice) {
    os << "[";
-   for (size_t i=0; i<matrice.size(); ++i) {
-      if (i)
+   for (Matrice::const_iterator it = matrice.begin(); it < matrice.end(); ++it) {
+      if (it != matrice.begin())
          os << ", ";
-      os << matrice[i];
+      os << *it;
    }
    os << "]";
    return os;
@@ -49,12 +53,15 @@ int minCol(const Matrice& matrice) {
 
 }
 
-vector<int> sommeLigne(const Matrice& matrice) {
+Vecteur1D sommeLigne(const Matrice& matrice) {
 
 }
 
-vector<int> sommeColonne(const Matrice& matrice) {
+Vecteur1D sommeColonne(const Matrice& matrice) {
+   if (estReguliere(matrice) && !matrice.empty())
+      return Vecteur1D();
 
+   return accumulate(matrice.begin(), matrice.end(), Vecteur1D(matrice[0].size()), sommeVecteur);
 }
 
 vector<int> vectSommeMin(const Matrice& matrice) {
@@ -67,4 +74,17 @@ void shuffleMatrice(const Matrice& matrice) {
 
 void sortMatrice(const Matrice& matrice) {
 
+}
+
+
+int addition(int a, int b) {
+   return a + b;
+}
+
+Vecteur1D sommeVecteur(const Vecteur1D& v1, const Vecteur1D& v2) {
+   Vecteur1D somme(v1.size());
+
+   transform(v1.begin(), v1.end(), v2.begin(), somme.begin(), addition);
+
+   return somme;
 }
